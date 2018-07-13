7d2ca3f3d078d3c7b2a4010c4598610997a5ace7
zig <zig1375@gmail.com>
2018-07-13 15:38:37 +0300

    0.9.13

--- Package.swift
+++ Package.swift
@@ -1,15 +1,21 @@
-//
-//  Package.swift
-//  libaerospike
-//
-//  Created by Sergey Gitinsky on 3/05/18.
-//	Copyright (C) 2018 Git in Sky
-//
-
+// swift-tools-version:4.1
 
 import PackageDescription
 
 let package = Package(
     name: "libaerospike",
-    pkgConfig: "libaerospike"
-)
+    products: [
+        .library(
+            name: "libaerospike",
+            targets: ["libaerospike"]
+        )
+    ],
+    dependencies: [
+
+    ],
+    targets: [
+        .target(
+            name: "libaerospike"
+        )
+    ]
+);
