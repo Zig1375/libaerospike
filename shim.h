256ac2f248105e9e772e206554094695e4ed9a39
zig <zig1375@gmail.com>
2018-08-20 17:32:43 +0300

    0.9.19

--- shim.h
+++ shim.h
@@ -1,3 +1,5 @@
 #include <aerospike/aerospike_key.h>
 #include <aerospike/as_record_iterator.h>
+#include <aerospike/as_iterator.h>
+#include <aerospike/as_map_iterator.h>
 #include <aerospike/as_operations.h>
