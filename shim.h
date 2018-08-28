dd9762179dd10abf1f0ca6d5cb523435482e9714
zig <zig1375@gmail.com>
2018-08-28 17:59:46 +0300

    0.9.24

--- shim.h
+++ shim.h
@@ -10,17 +10,3 @@
 extern inline as_status aerospike_key_apply2(aerospike* as, as_error* err, const as_key* key, const char* module, const char* function, as_arraylist* arglist, as_val** result) {
     return aerospike_key_apply(as, err, NULL, key, module, function, arglist, result);
 }
-
-extern inline void test(aerospike* as, as_error* err) {
-    as_key key;
-    as_key_init(&key, "dmp", "test", "218");
-    as_arraylist args;
-    as_arraylist_inita(&args, 2);
-    as_arraylist_append_int64(&args, 1);
-    as_arraylist_append_int64(&args, 2);
-    as_val * res = NULL;
-    if (aerospike_key_apply(&as, &err, NULL, &key, "myudf", "hello_world2", &args, &res) == AEROSPIKE_OK) {
-        printf("a: %d", as_integer_get( as_integer_fromval(res) ));
-    }
-    as_arraylist_destroy(&args);
-}
