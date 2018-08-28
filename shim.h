5a1591ef00a8b418766a32071869a5076f1a12b7
zig <zig1375@gmail.com>
2018-08-28 18:51:59 +0300

    0.9.24

--- shim.h
+++ shim.h
@@ -6,6 +6,7 @@
 #include <aerospike/as_operations.h>
 #include <aerospike/aerospike_udf.h>
 #include <aerospike/as_udf.h>
+#include <aerospike/as_log.h>
 
 extern inline as_status aerospike_key_apply2(aerospike* as, as_error* err, const as_key* key, const char* module, const char* function, as_arraylist* arglist, as_val** result) {
     return aerospike_key_apply(as, err, NULL, key, module, function, arglist, result);
