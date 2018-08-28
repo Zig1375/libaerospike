e9fa287d68cadc44bbf7c3743031feada9223616
zig <zig1375@gmail.com>
2018-08-28 18:57:27 +0300

    0.9.24

--- shim.h
+++ shim.h
@@ -11,3 +11,22 @@
 extern inline as_status aerospike_key_apply2(aerospike* as, as_error* err, const as_key* key, const char* module, const char* function, as_arraylist* arglist, as_val** result) {
     return aerospike_key_apply(as, err, NULL, key, module, function, arglist, result);
 }
+
+bool my_log_callback(as_log_level level, const char *func, const char *file, uint32_t line, const char *fmt, ...) {
+    char msg[1024] = {0};
+    va_list ap;
+
+    va_start(ap, fmt);
+    vsnprintf(msg, 1024, fmt, ap);
+    msg[1023] = '\0';
+    va_end(ap);
+
+    fprintf(stderr, "[%s:%d][%s] %d - %s\n", file, line, func, level, msg);
+
+    return true;
+}
+
+extern inline void log_enabler() {
+    as_log_set_level(AS_LOG_LEVEL_INFO);
+    as_log_set_callback(my_log_callback);
+}
