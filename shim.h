8cb554476edbca48665c5e3e03044869ed2724b0
zig <zig1375@gmail.com>
2018-10-04 14:44:01 +0300

    0.9.40

--- shim.h
+++ shim.h
@@ -8,6 +8,6 @@
 #include <aerospike/as_udf.h>
 #include <aerospike/as_log.h>
 
-extern inline as_status aerospike_key_apply2(aerospike* as, as_error* err, const as_key* key, const char* module, const char* function, as_arraylist* arglist, as_val** result) {
-    return aerospike_key_apply(as, err, NULL, key, module, function, arglist, result);
+extern inline as_status aerospike_key_apply2(aerospike* as, as_error* err, const as_policy_apply* policy, const as_key* key, const char* module, const char* function, as_arraylist* arglist, as_val** result) {
+    return aerospike_key_apply(as, err, policy, key, module, function, arglist, result);
 }
